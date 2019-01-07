d8840b58003b80b704af36a16d5588e47a46707a
Leyla <LDuymazlar@SAC.local>
2019-01-06 20:27:35 -0600

    random code in console, modal not working

--- public/js/event.js
+++ public/js/event.js
@@ -1,32 +1,6 @@
 $(document).ready(function () {
    
-    // console.log('hello');
-    // //Activate modal
-    // $("#create-event-butt").click(function () {
-    //     $("#ModalGroupForm").modal();
-  
-    // });
-
-    // function randomString() {
-    //     console.log("in random function");
-    //     var _options = xtend({
-    //         chars: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    //         parts: 1,
-    //         part_size: 4,
-    
-    //         getChar: function (pool) {
-    //             var random = Math.floor(Math.random() * pool.length);
-    //             return pool.charAt(random);
-    //             console.log(random);
-    //         }
-    //     });
-        //or even this as you are using JQuery
-        // $('#randomfield').val(randomstring);
-        // console.log(randomString);
-        // console.log(randomString());
-       
-    
-
+   
     //create card on create event
     $("#submitEventButt").click(function () {
         console.log("in submit function");
