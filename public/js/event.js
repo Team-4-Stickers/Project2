ee805922036f609ae05b493351012d62d35996ef
Kinky Cookie <kuki_kukula@yahoo.com>
2019-01-07 16:58:06 -0600

    modal fixed for code pop up

--- public/js/event.js
+++ public/js/event.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-   
-   
+
     //create card on create event
     $("#submitEventButt").click(function () {
+        event.preventDefault();
         console.log("in submit function");
-        var eventName = $("#event-name").val();
-         console.log(eventName);
-         var val = Math.floor(1000 + Math.random() * 9000);
-       console.log(val);
-       $(".modal-body").text(val);
-        
+        // var eventName = $("#event-name").val();
+        // console.log(eventName);
+        var val = Math.floor(1000 + Math.random() * 9000);
+        console.log(val);
+        $(".modal-body").text(val);
+        $("#exampleModal").modal("show");
+    });
 });
-});
-
