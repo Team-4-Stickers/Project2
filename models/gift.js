fadec7adc8ca3e42de9820b2023b820b6a892b0e
melwell027 <melwell1027@Gmail.com>
2019-01-09 15:57:34 -0600

    adding updated routes and updated model

--- models/gift.js
+++ models/gift.js
@@ -1,25 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    const Gift = sequelize.define('Gift', {
-  
-      giftName: {
-        type: DataTypes.TEXT,
-        required: true
-      },
-      giftPrice: {
-        type: DataTypes.DECIMAL,
-        required: true
-      },
-      giftStatus: {
-        type: DataTypes.BOOLEAN,
-        required: true
-      },
-      buying: {
-        type: DataTypes.BOOLEAN,
-        required: true
-      },
-     
-  
-    });
-    console.log(Gift); 
-    return Gift;
-  };
+  const Gift = sequelize.define('Gift', {
+
+    giftName: {
+      type: DataTypes.TEXT,
+      required: true
+    },
+    priceRange: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    giftLink: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    giftComment: {
+      type: DataTypes.STRING,
+      required: false
+    },
+    giftStatus: {
+      type: DataTypes.BOOLEAN,
+      required: true,
+      default: false
+    }
+  });
+  console.log(Gift);
+  return Gift;
+};
